747d36f5f13cb254bff55910ccd55cf71122a953
Lawrence <lthomas@administrators-iMac.local>
2024-04-08 15:43:36 -0400

    updates search results

--- search.php
+++ search.php
@@ -9,45 +9,64 @@
 
 get_header();
 ?>
+	<div class="c-page-search-results">
+	<div class="content">
+
 
 	<main id="primary" class="site-main">
 
 		<?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
-				<h1 class="page-title">
+	<?php function custom_search_results_count() {
+    global $wp_query;
+    
+    $total_results = $wp_query->found_posts;
+
+    if ($total_results > 1) {
+        echo $total_results . ' results';
+    } elseif ($total_results == 1) {
+        echo $total_results . ' result';
+    } else {
+        echo '<p>No results found</p>';
+    }
+} ?>
+
+			<header class="c-page-search-results__header">
+				<h1 class="c-page-search-results__page-title">
 					<?php
-					/* translators: %s: search query. */
 					printf( esc_html__( 'Search Results for: %s', 'no-cred-podcast' ), '<span>' . get_search_query() . '</span>' );
 					?>
 				</h1>
+				<p class="c-page-search-results__number"><?php custom_search_results_count(); ?></p>
 			</header><!-- .page-header -->
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
 
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
+	
+			<ul class="c-page-search-results__list">
+			<?php while ( have_posts() ) : the_post(); ?>
+			<a class="c-page-search-results__list-item-cta" href="<?php echo the_permalink(); ?>">
+			<li class="c-page-search-results__list-item">
+				<figure>
+					<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
+				</figure>
+				<div class="c-page-search-results__list-item-content">
+					<h2 class="c-page-search-results__list-item-title"><?php the_title(); ?></h2>
+					<p class="c-page-search-results__list-item-desc"><?php echo get_the_excerpt(); ?></p>
 
-			endwhile;
+					<p class="c-page-search-results__list-item-air-date"><span>Air Date:</span><?php echo get_field('air_date', $post->ID); ?></p>
+				</div>
+			</li>
+			</a>
 
-			the_posts_navigation();
 
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
+			<?php endwhile; ?>
+			</ul>
+		<?php endif; ?>
 
 	</main><!-- #main -->
+	</div> <!-- content -->
+	</div> <!-- Search Results -->
 
-<?php
-get_sidebar();
-get_footer();
+	</div>
+
+<?php get_footer(); ?>
